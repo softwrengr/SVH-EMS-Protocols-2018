ad87bffe89a87968deff1cbc396cf4686a88f3d1
eapple <softwrengr@gmail.com>
2018-12-23 11:52:59 +0500

    pdf added

--- app/src/main/java/com/eagledeveloper/svhems/ui/fragments/PDFFragment.java
+++ app/src/main/java/com/eagledeveloper/svhems/ui/fragments/PDFFragment.java
@@ -9,15 +9,28 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.eagledeveloper.svhems.R;
+import com.github.barteksc.pdfviewer.PDFView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class PDFFragment extends Fragment {
    View view;
+
+    @BindView(R.id.pdfView)
+    PDFView pdfView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_pdf, container, false);
+        initUI();
         return view;
+    }
+
+    private void initUI(){
+        ButterKnife.bind(this,view);
+
     }
 }
